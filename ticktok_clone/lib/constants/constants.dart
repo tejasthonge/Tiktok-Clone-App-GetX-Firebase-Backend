0827658765582a891e42edc7e9e6370cd50a6c72
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2024-07-23 07:50:19 +0530

    creting all the firebase constant object in contant file

--- ticktok_clone/lib/constants/constants.dart
+++ ticktok_clone/lib/constants/constants.dart
@@ -1,8 +1,19 @@
 
 
 // COLORS
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 const backgroundColor = Colors.black;
 var buttonColor = Colors.red[400];
 const borderColor = Colors.grey;
+
+
+//Firebase constants objects 
+
+FirebaseAuth auth = FirebaseAuth.instance;
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+FirebaseStorage firestoreStorage = FirebaseStorage.instance;
+

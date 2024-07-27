2b3b91bc2a6d42c851de78594d402ce00d57f33f
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2024-07-27 08:22:09 +0530

    .

--- ticktok_clone/lib/controllers/videos/comment_controller.dart
+++ ticktok_clone/lib/controllers/videos/comment_controller.dart
@@ -1,3 +1,33 @@
 
 
 
+
+
+import 'package:get/get.dart';
+import 'package:ticktok_clone/models/comment_modal.dart';
+
+class CommentController extends GetxController{ 
+
+  final Rx<List<CommentModel>> _listComment =Rx<List<CommentModel>>([]);
+
+  List<CommentModel> get listComment=> _listComment.value;
+
+  String _postID = "";
+  updatePostID({required String postId}){
+    _postID = postId;
+    getComment();
+  }
+
+  getComment()async{
+
+  }
+
+  //
+  postComment({
+    required String commentText,
+  })async{
+
+    
+  }
+
+}
